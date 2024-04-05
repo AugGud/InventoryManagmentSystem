e3c462c9a9871f38f9df4490df8d8d05f0429800
Augustas <augustasgudynas@gmail.com>
2024-04-06 00:56:22 +0300

    Removed unused code

--- src/main/java/com/auggud/InventoryManagmentSystem/InventoryItemDTO.java
+++ src/main/java/com/auggud/InventoryManagmentSystem/InventoryItemDTO.java
@@ -54,7 +54,8 @@ public class InventoryItemDTO {
         this.amount = amount;
     }
 
-    public InventoryItemDTO(String name, String description, int quantity, BigDecimal amount) {
+    public InventoryItemDTO(Long id, String name, String description, int quantity, BigDecimal amount) {
+        this.id = id;
         this.name = name;
         this.description = description;
         this.quantity = quantity;
@@ -62,52 +63,6 @@ public class InventoryItemDTO {
     }
 
     public InventoryItemDTO() {
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((description == null) ? 0 : description.hashCode());
-        result = prime * result + quantity;
-        result = prime * result + ((amount == null) ? 0 : amount.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        InventoryItemDTO other = (InventoryItemDTO) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        if (description == null) {
-            if (other.description != null)
-                return false;
-        } else if (!description.equals(other.description))
-            return false;
-        if (quantity != other.quantity)
-            return false;
-        if (amount == null) {
-            if (other.amount != null)
-                return false;
-        } else if (!amount.equals(other.amount))
-            return false;
-        return true;
     }
 
     @Override
