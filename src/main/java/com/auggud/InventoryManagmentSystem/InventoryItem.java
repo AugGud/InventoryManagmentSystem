b3e655fc8ed2286b88ecb53be1f893839d77c693
Augustas <augustasgudynas@gmail.com>
2024-04-06 00:55:18 +0300

    Removed unused methods.

--- src/main/java/com/auggud/InventoryManagmentSystem/InventoryItem.java
+++ src/main/java/com/auggud/InventoryManagmentSystem/InventoryItem.java
@@ -81,52 +81,6 @@ class InventoryItem {
   }
 
   @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    result = prime * result + ((name == null) ? 0 : name.hashCode());
-    result = prime * result + ((description == null) ? 0 : description.hashCode());
-    result = prime * result + quantity;
-    result = prime * result + ((amount == null) ? 0 : amount.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    InventoryItem other = (InventoryItem) obj;
-    if (id == null) {
-      if (other.id != null)
-        return false;
-    } else if (!id.equals(other.id))
-      return false;
-    if (name == null) {
-      if (other.name != null)
-        return false;
-    } else if (!name.equals(other.name))
-      return false;
-    if (description == null) {
-      if (other.description != null)
-        return false;
-    } else if (!description.equals(other.description))
-      return false;
-    if (quantity != other.quantity)
-      return false;
-    if (amount == null) {
-      if (other.amount != null)
-        return false;
-    } else if (!amount.equals(other.amount))
-      return false;
-    return true;
-  }
-
-  @Override
   public String toString() {
     return "InventoryItem [id=" + id + ", name=" + name + ", description=" + description + ", quantity=" + quantity
         + ", amount=" + amount + "]";
